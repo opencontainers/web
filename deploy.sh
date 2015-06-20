1e44237a542533254efd5c8596182a0e5c36aa6c
Patrick Chanezon <patlist@chanezon.com>
2015-06-20 06:18:00 +0100

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,20 @@
 # Install go if needed
-TOOLS_ROOT_DIR=$HOME/site/deployments/tools
+export TOOLS_ROOT_DIR=$HOME/site/deployments/tools
 export HUGOROOT=$TOOLS_ROOT_DIR/hugo
 export PATH=$PATH:$HUGOROOT
 #export GOPATH=$DEPLOYMENT_SOURCE
 if [ ! -e "$HUGOROOT" ]; then
-  HUGO_ARCHIVE_DIR=$HOME/tmp
-  HUGO_ARCHIVE=${HUGO_ARCHIVE_DIR}/hugo.zip
+  export HUGO_ARCHIVE_DIR=$HOME/tmp
+  export HUGO_ARCHIVE=${HUGO_ARCHIVE_DIR}/hugo.zip
   mkdir -p ${HUGO_ARCHIVE_DIR}
   curl -L https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_windows_amd64.zip -o $HUGO_ARCHIVE
   # This will take a while ...
   unzip -o $HUGO_ARCHIVE -d $HUGOROOT
 fi
 
-BASE_URL = "https://$WEBSITE_HOSTNAME"
+export BASE_URL="https://$WEBSITE_HOSTNAME"
 if [ "$WEBSITE_HOSTNAME" == "opencontainers.azurewebsites.net" ]; then
-  BASE_URL = "https://www.opencontainers.org"
+  export BASE_URL="https://www.opencontainers.org"
 fi
 
 # Create and store unique artifact name
