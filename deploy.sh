92a2db17fbb81325f2963c12c612e136ae9fd589
Patrick Chanezon <patlist@chanezon.com>
2015-06-20 02:39:35 +0100

    fixed issues in templates and deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,13 @@ export HUGOROOT=$HOME/hugo
 export PATH=$PATH:$HUGOROOT
 #export GOPATH=$DEPLOYMENT_SOURCE
 if [ ! -e "$HUGOROOT" ]; then
-  HUGO_ARCHIVE=$HOME/tmp/hugo.zip
-  mkdir -p ${HUGO_ARCHIVE%/*}
+  HUGO_ARCHIVE_DIR=$HOME/tmp
+  HUGO_ARCHIVE=${HUGO_ARCHIVE_DIR}/hugo.zip
+  mkdir -p ${HUGO_ARCHIVE_DIR}
   curl https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_windows_amd64.zip -o $HUGO_ARCHIVE
   # This will take a while ...
   unzip $HUGO_ARCHIVE -d $HOME
 fi
 
 # Create and store unique artifact name
-hugo_0.14_windows_amd64.exe
+hugo_0.14_windows_amd64.exe -d $DEPLOYMENT_TARGET
