c176b1ed0a924313d837005fd15ad6a419c560c7
Patrick Chanezon <patlist@chanezon.com>
2015-06-20 03:05:27 +0100

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ if [ ! -e "$HUGOROOT" ]; then
   mkdir -p ${HUGO_ARCHIVE_DIR}
   curl -L https://github.com/spf13/hugo/releases/download/v0.14/hugo_0.14_windows_amd64.zip -o $HUGO_ARCHIVE
   # This will take a while ...
-  unzip $HUGO_ARCHIVE -d $TOOLS_ROOT_DIR
+  unzip -o $HUGO_ARCHIVE -d $HUGOROOT
 fi
 
 # Create and store unique artifact name
